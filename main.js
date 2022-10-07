5c36eef8e7fa8814e5a9c38a0a9d4ef0f6c43c76
Shea Ehrenberger <sheae123@gmail.com>
2022-10-07 15:59:11 -0700

    html tile reformat and renderBoard function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const HIDDENTILE = 'imgs/star.png';
 
 /*----- app's state (variables) -----*/
-let tileboard; // object holding the 16 tiles with their properties set to the value of background color they will have, contains matching tiles
+let tileBoard; // object holding the 16 tiles with their properties set to the value of background color they will have, contains matching tiles
 let selectedTile1; // will hold the value of 1 of 2 of tiles clicked
 let selectedTile2; // will hold value of other tile clicked
 let winner; // will be set to true if all tiles are matched by end of game
@@ -19,22 +19,22 @@ init();
 
 function init() {
     tileBoard = [
-        {'tile1': 'orange'},
-        {'tile2': 'orange'},
-        {'tile3': 'pink'},
-        {'tile4': 'pink'},
-        {'tile5': 'green'},
-        {'tile6': 'green'}, 
-        {'tile7': 'yellow'}, 
-        {'tile8': 'yellow'},
-        {'tile9': 'blue'}, 
-        {'tile10': 'blue'},
-        {'tile11': 'red'}, 
-        {'tile12': 'red'},
-        {'tile13': 'gray'},
-        {'tile14': 'gray'}, 
-        {'tile15': 'purple'},
-        {'tile16': 'purple'},
+        {'tile': 0, 'color': 'orange'},
+        {'tile': 1, 'color': 'orange'},
+        {'tile': 2, 'color': 'pink'},
+        {'tile': 3, 'color': 'pink'},
+        {'tile': 4, 'color': 'green'},
+        {'tile': 5, 'color': 'green'},
+        {'tile': 6, 'color': 'yellow'},
+        {'tile': 7, 'color': 'yellow'},
+        {'tile': 8, 'color': 'blue'},
+        {'tile': 9, 'color': 'blue'},
+        {'tile': 10, 'color': 'red'},
+        {'tile': 11, 'color': 'red'},
+        {'tile': 12, 'color': 'gray'},
+        {'tile': 13, 'color': 'gray'},
+        {'tile': 14, 'color': 'purple'},
+        {'tile': 15, 'color': 'purple'},
     ];
     matched = false;
     winner = false;
@@ -48,7 +48,11 @@ function handleClick(evt) {
 }
 
 function renderBoard() {
-
+    for (let idx in tileBoard) { // idx returns the index of tileBoard array
+        const tileId = `tile${idx}`;
+        const tile = document.getElementById(tileId);
+       tile.style.backgroundColor = tileBoard[idx].color;
+    }
 }
 
 function render() {
