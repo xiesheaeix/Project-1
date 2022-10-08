089d7f7d05a7b51ba40aec0f22948b1949d5e91c
Shea Ehrenberger <sheae123@gmail.com>
2022-10-08 11:17:16 -0700

    shuffle tile & handle click logic

--- main.js
+++ main.js
@@ -9,49 +9,67 @@ let winner; // will be set to true if all tiles are matched by end of game
 let matched; // will be set to true or false depending on if properties of selectedTile1 & 2 are equal to each other
 
 /*----- cached element references -----*/
-
+const board = document.getElementById('board');
+const button = document.getElementById('button');
 
 /*----- event listeners -----*/
-document.getElementById('board').addEventListener('click', handleClick);
+board.addEventListener('click', handleClick);
+//button.addEventListener('click', init);
 
 /*----- functions -----*/
 init();
 
 function init() {
     tileBoard = [
-        {'tile': 0, 'color': 'orange'},
-        {'tile': 1, 'color': 'orange'},
-        {'tile': 2, 'color': 'pink'},
-        {'tile': 3, 'color': 'pink'},
-        {'tile': 4, 'color': 'green'},
-        {'tile': 5, 'color': 'green'},
-        {'tile': 6, 'color': 'yellow'},
-        {'tile': 7, 'color': 'yellow'},
-        {'tile': 8, 'color': 'blue'},
-        {'tile': 9, 'color': 'blue'},
-        {'tile': 10, 'color': 'red'},
-        {'tile': 11, 'color': 'red'},
-        {'tile': 12, 'color': 'gray'},
-        {'tile': 13, 'color': 'gray'},
-        {'tile': 14, 'color': 'purple'},
-        {'tile': 15, 'color': 'purple'},
+        {'tile': 0, 'color': 'orange', 'show': false},
+        {'tile': 1, 'color': 'orange', 'show': false},
+        {'tile': 2, 'color': 'pink', 'show': false},
+        {'tile': 3, 'color': 'pink', 'show': false},
+        {'tile': 4, 'color': 'green', 'show': false},
+        {'tile': 5, 'color': 'green', 'show': false},
+        {'tile': 6, 'color': 'yellow', 'show': false},
+        {'tile': 7, 'color': 'yellow', 'show': false},
+        {'tile': 8, 'color': 'blue', 'show': false},
+        {'tile': 9, 'color': 'blue', 'show': false},
+        {'tile': 10, 'color': 'red', 'show': false},
+        {'tile': 11, 'color': 'red', 'show': false},
+        {'tile': 12, 'color': 'gray', 'show': false},
+        {'tile': 13, 'color': 'gray', 'show': false},
+        {'tile': 14, 'color': 'purple', 'show': false},
+        {'tile': 15, 'color': 'purple', 'show': false},
     ];
     matched = false;
     winner = false;
+    shuffleTiles();
     render();
 }
 
 
 function handleClick(evt) {
-
-
+let curTile = evt.target.id.match(/\d+/g);
+if(curTile === 'board') return;
+tileBoard[curTile].show = true;
+render();
 }
 
+
+function shuffleTiles() {
+    let curIdx = tileBoard.length;
+    while (curIdx--) {
+       let rand =  Math.floor(Math.random() * curIdx);
+       [tileBoard[curIdx], tileBoard[rand]] = [tileBoard[rand], tileBoard[curIdx]];
+    }
+    return tileBoard;
+}
+
+
 function renderBoard() {
-    for (let idx in tileBoard) { // idx returns the index of tileBoard array
-        const tileId = `tile${idx}`;
-        const tile = document.getElementById(tileId);
-       tile.style.backgroundColor = tileBoard[idx].color;
+    for (let i = 0; i < tileBoard.length; i++) { // idx returns the index of tileBoard array
+        const tileId = `tile${i}`;
+        const tileEl = document.getElementById(tileId);
+        tileBoard[i].show ? 
+        tileEl.style.backgroundColor = tileBoard[i].color : 
+        tileEl.style.backgroundColor = 'white';
     }
 }
 
